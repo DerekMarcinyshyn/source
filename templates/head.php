9e3520f6195102fe5f987bc079d49a142f147b50
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2013-01-22 16:15:09 -0800

    add links in upper bar

--- templates/head.php
+++ templates/head.php
@@ -4,18 +4,20 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
-  <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
 
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
 
-  <?php wp_head(); ?>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
 
-  <?php if (wp_count_posts()->publish > 0) : ?>
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
-  <?php endif; ?>
+    <?php wp_head(); ?>
+
+    <?php if (wp_count_posts()->publish > 0) : ?>
+        <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+    <?php endif; ?>
 </head>
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,4 +1,19 @@
-<header id="banner" class="navbar navbar-fixed-top" role="banner">
+<div class="upper">
+    <div class="container">
+        <div class="brand-container">
+            <a class="brand" href="<?php echo home_url(); ?>/">
+                <img src="<?php echo get_bloginfo('wpurl'); ?>/assets/img/source.png" width="334" height="124" alt="The Source Skate and Snowboard Shop"/>
+            </a>
+        </div>
+        <div class="brand-text">
+            <div class="facebook"><a href="https://www.facebook.com/sourceboards"><img src="<?php echo get_bloginfo('wpurl'); ?>/assets/img/facebook.jpg" alt="Facebook" />/sourceboards</a></div>
+            <div class="sourceboards"><a href="http://www.sourceboards.com">www.sourceboards.com</a><br />toll free 1-877-576-8723</div>
+            <div class="twitter"><a href="https://twitter.com/sourceboards"><img src="<?php echo get_bloginfo('wpurl'); ?>/assets/img/twitter.jpg" alt="Twitter" />#sourceboards</a></div>
+        </div>
+    </div>
+</div>
+
+<header id="banner" class="navbar" role="banner">
   <div class="navbar-inner">
     <div class="container">
       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -6,9 +21,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
-      <a class="brand" href="<?php echo home_url(); ?>/">
-          <img src="<?php echo get_bloginfo('wpurl'); ?>/assets/img/logo.png" width="170" height="64" />
-      </a>
+
       <nav id="nav-main" class="nav-collapse" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
